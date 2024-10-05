3bf12c0f37af9ba6361ebca41c08a83ce7e4f377
lokodata <lokodata115@gmail.com>
2024-10-05 13:37:12 +0800

    landing page improvement + navigation for notes

--- laravel-share-notes/resources/views/notes/index.blade.php
+++ laravel-share-notes/resources/views/notes/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-            {{ __('Dashboard') }}
+            {{ __('Notes') }}
         </h2>
     </x-slot>
 
@@ -9,7 +9,7 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <x-button primary>Hi There!</x-button>
+                    <x-button primary>Hi There Notes!</x-button>
                 </div>
             </div>
         </div>
